60c0dd1e43466500e6d4b4a08dc17793a5245f73
Rosy Stephen <rosystephen64@gmail.com>
2022-11-17 23:43:21 +0530

    DB changes

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=req',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=recruitment',
+    'username' => 'rosystephen',
+    'password' => 'gWwwABmIwBuQYvB',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
